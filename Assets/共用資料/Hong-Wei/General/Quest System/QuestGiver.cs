9e82df263021e2204363046ef9667864972aa6f3
Blackteatime <138540261+Blackteatime@users.noreply.github.com>
2023-09-10 22:56:13 +0800

    Update 任務視窗1.11

--- Assets/共用資料/Hong-Wei/General/Quest System/QuestGiver.cs
+++ Assets/共用資料/Hong-Wei/General/Quest System/QuestGiver.cs
@@ -8,50 +8,34 @@ using UnityEngine.UI;
 public class QuestGiver : MonoBehaviour
 {
     public Quest quest;
-    public QuestGoal goal;
-
     public Player player;
 
-    public GameObject questWindow;
     public TextMeshProUGUI titleText;
     public TextMeshProUGUI descriptionText;
     public TextMeshProUGUI goldText;
-    public GameObject guidanceWindow;
     public TextMeshProUGUI guideTitleText;
     public TextMeshProUGUI guideDescriptionText;
     
-    private void Start()
-    {
-        
-    }
     private void Update()
     {
         ChangeGuidanceState();
     }
     public void OpenQuestWindow()
     {
-        questWindow.SetActive(true);
         titleText.text = quest.title;
         descriptionText.text = quest.description;
         goldText.text = quest.goldReward.ToString();
     }
 
-    public void CloseQuestWindow()
-    {
-        questWindow.SetActive(false);
-    }
-
     public void AcceptQuest()
     {
-        questWindow.SetActive(false);
         quest.isActive = true;
         player.quest = quest;
-        guidanceWindow.SetActive(true);
         guideTitleText.text=quest.title;
         ChangeGuidanceState();
     }
     public void ChangeGuidanceState()
     {
-        guideDescriptionText.text = Convert.ToString($"Kill enemy:{goal.currentAmount}/{goal.requiredAmount}");
+        guideDescriptionText.text = Convert.ToString($"Kill enemy:{quest.goal.currentAmount}/{quest.goal.requiredAmount}");
     }
 }
