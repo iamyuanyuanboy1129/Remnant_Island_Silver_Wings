9033a463e560801363e065ac8155256cf4f6b871
HsuanShi <138540116+HsuanShi@users.noreply.github.com>
2023-09-20 14:59:09 +0800

    Complete 躲藏系統

---
+++ Assets/共用資料/Script/HidingObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwoD
+{
+    public class HidingObject : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            collision.GetComponent<Player>().canHide = true;
+        }
+    }
+
+}
