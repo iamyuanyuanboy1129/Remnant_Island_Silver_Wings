e8309f85ca3e0de7752b70558393c5172f18ecda
HsuanShi <138540116+HsuanShi@users.noreply.github.com>
2023-09-21 11:40:47 +0800

    Update ChangeSceneAfterDead.cs

--- Assets/共用資料/Script/Enemy/ChangeSceneAfterDead.cs
+++ Assets/共用資料/Script/Enemy/ChangeSceneAfterDead.cs
@@ -6,16 +6,25 @@ namespace TwoD
 {
     public class ChangeSceneAfterDead : MonoBehaviour
     {
+        private GameObject gameObject;
         // Start is called before the first frame update
         void Start()
         {
-
+            gameObject = GameObject.Find(this.name);
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            if (gameObject.GetComponent<HealthSystem>().currentHealth == 0)
+            {
+                StartCoroutine(WaitToChangeScene());
+            }
+        }
+        IEnumerator WaitToChangeScene()
+        {
+            yield return new WaitForSeconds(3);
+            UnityEngine.SceneManagement.SceneManager.LoadScene("皇宮");
         }
     }
 }
