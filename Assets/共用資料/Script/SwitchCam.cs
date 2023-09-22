96f3194c4825f6e655d2af4af0e08a14ce5eecb0
HsuanShi <138540116+HsuanShi@users.noreply.github.com>
2023-09-22 14:47:48 +0800

    Update Changing Camera Priority

---
+++ Assets/共用資料/Script/SwitchCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class SwitchCam : MonoBehaviour
+{
+    public CinemachineVirtualCamera vC2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vC2 = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeToHigherPriority()
+    {
+        vC2.Priority = 15;
+    }
+    public void ChangeToLowerPriority()
+    {
+        vC2.Priority = 5;
+    }
+}
