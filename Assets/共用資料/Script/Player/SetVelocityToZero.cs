51d1be0f177d655e1215756069722f98a55b285e
HsuanShi <138540116+HsuanShi@users.noreply.github.com>
2023-09-25 10:11:22 +0800

    Update 速度歸零Script

---
+++ Assets/共用資料/Script/Player/SetVelocityToZero.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwoD
+{
+    public class SetVelocityToZero : MonoBehaviour
+    {
+        GameObject gameObject;
+        Rigidbody2D rig;
+        // Start is called before the first frame update
+        void Start()
+        {
+            gameObject = GameObject.Find("Player_Idle");
+            rig = gameObject.GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetV2Z()
+        {
+            rig.velocity = Vector3.zero;
+        }
+    }
+}
