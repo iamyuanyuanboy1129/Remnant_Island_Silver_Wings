b4466b65a4a521bf096de11dea45de465fd23b3a
Blackteatime <138540261+Blackteatime@users.noreply.github.com>
2023-10-15 22:23:54 +0800

    Update 電

---
+++ Assets/共用資料/Hong-Wei/General/Platform/PlatformMoveSmooth.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMoveSmooth : MonoBehaviour
+{
+    [SerializeField, Header("初始座標")]
+    private Vector3 pointOriginal;
+    [SerializeField, Header("終點")]
+    private Vector3 pointEnd;
+    [SerializeField, Header("移動速度"), Range(0, 1)]
+    private float speed;
+
+    private float sinTime;
+
+    private void Start()
+    {
+        pointOriginal = transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+        Swap();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0.2f, 0.8f, 0.9f, 0.7f);
+        Gizmos.DrawSphere(pointEnd, 0.5f);
+    }
+
+    private void Move()
+    {
+        if (transform.position != pointEnd)
+        {
+            sinTime += speed * Time.deltaTime;
+            sinTime = Mathf.Clamp(sinTime, 0, Mathf.PI);
+            float t = Evaluate(sinTime);
+            transform.position = Vector3.Lerp(transform.position, pointEnd, t);
+        }
+    }
+
+    private void Swap()
+    {
+        if (transform.position != pointEnd)
+        {
+            return;
+        }
+        Vector3 t = pointOriginal;
+        pointOriginal = pointEnd;
+        pointEnd = t;
+        sinTime = 0;
+    }
+
+    private float Evaluate(float x)
+    {
+        return 0.5f * Mathf.Sin(x - Mathf.PI / 2) + 0.5f;
+    }
+}
---
+++ Assets/共用資料/Hong-Wei/General/Trap/TrapRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapRotation : MonoBehaviour
+{
+    [SerializeField, Header("中心點")]
+    private GameObject center;
+    [SerializeField, Header("轉動速度"), Range(10, 100)]
+    private float rotationSpeed = 50;
+    [SerializeField, Header("是否順時針")]
+    private bool isClockWise = true;
+
+    private void Update()
+    {
+        transform.RotateAround(center.transform.position, isClockWise ? Vector3.back : Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
