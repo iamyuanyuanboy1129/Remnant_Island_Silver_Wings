477c02c23a4dcad33d8f23e8a6d967bf12661b3d
Blackteatime <138540261+Blackteatime@users.noreply.github.com>
2023-10-18 15:56:41 +0800

    Update iTween

---
+++ Assets/共用資料/Hong-Wei/General/Trap/TrapSwing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSwing : MonoBehaviour
+{
+    [SerializeField] private float leftLimit = 0.3f;
+    [SerializeField] private float rightLimit = 0.3f;
+    [SerializeField] private float speed;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.angularVelocity = 500;
+    }
+
+    private void Update()
+    {
+        SwingMove();
+    }
+
+    private void SwingMove()
+    {
+        if (transform.rotation.z < rightLimit && rb.angularVelocity > 0 && rb.angularVelocity < speed)
+        {
+            rb.angularVelocity = speed;
+        }
+        if (transform.rotation.z > leftLimit && rb.angularVelocity < 0 && rb.angularVelocity > -speed)
+        {
+            rb.angularVelocity = -speed;
+        }
+    }
+}
