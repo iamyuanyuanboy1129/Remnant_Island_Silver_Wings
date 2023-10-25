3eed7535233ac323d4ed34b297f23e2247b1021e
HsuanShi <138540116+HsuanShi@users.noreply.github.com>
2023-10-25 16:14:37 +0800

    Update Boss System

---
+++ Assets/共用資料/Script/Boss/StateBossJumpAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace TwoD
+{
+
+    public class StateBossJumpAttack : State
+    {
+        [SerializeField, Header("跳躍力道x"), Range(0, 5000)]
+        private float jumpForceX = 50f;
+        [SerializeField, Header("跳躍力道y"), Range(0, 5000)]
+        private float jumpForceY = 500f;
+
+        public StateBossTrack bossTrack;
+
+        private Rigidbody2D rig;
+        private void Start()
+        {
+            rig = GetComponent<Rigidbody2D>();
+        }
+        public override State RunCurrentState()
+        {
+            JumpAttack();
+            return bossTrack;
+        }
+        private void JumpAttack()
+        {
+            rig.AddForce(new Vector2(jumpForceX * bossTrack.direction, jumpForceY));
+            ani.SetTrigger("觸發跳躍攻擊");
+        }
+    }
+}
