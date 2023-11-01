4f4d0abd7097cfd6a436b21aa7bed159eb4ad5d4
HsuanShi <138540116+HsuanShi@users.noreply.github.com>
2023-11-01 11:53:48 +0800

    Complete Boss UI

---
+++ Assets/共用資料/Script/UI/SetBossUI.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+namespace TwoD
+{
+    public class SetBossUI : MonoBehaviour
+    {
+        public Vector3 pointOriginal;
+
+        [SerializeField, Header("Boss血條")]
+        private GameObject bossHealthBar;
+        [SerializeField, Header("Boss名稱")]
+        private GameObject bossName;
+
+        [field: SerializeField, Header("目標圖層")]
+        protected LayerMask layerTarget { get; private set; }
+
+        [Header("Boss範圍")]
+        [SerializeField]
+        private Vector3 roomSize = Vector3.one;
+        [SerializeField]
+        private Vector3 roomOffset;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(0f, 1f, 0f, 0.1f);
+            Gizmos.DrawCube(pointOriginal + transform.TransformDirection(roomOffset), roomSize);
+        }
+
+        void Update()
+        {
+            if(PlayerInRoom())
+            {
+                print("玩家進入房間");
+                bossHealthBar.SetActive(true);
+                bossName.SetActive(true);
+            }
+            else
+            {
+                bossHealthBar.SetActive(false);
+                bossName.SetActive(false);
+            }
+        }
+        public bool PlayerInRoom()
+        {
+            Collider2D hit = Physics2D.OverlapBox(pointOriginal + transform.TransformDirection(roomOffset), roomSize, 0, layerTarget);
+            return hit;
+        }
+        [ContextMenu("取得原始座標")]
+        private void GetOriginalPoint()
+        {
+            pointOriginal = transform.position;
+        }
+    }
+}
--- Assets/共用資料/Script/UI/UIManager.cs
+++ Assets/共用資料/Script/UI/UIManager.cs
@@ -8,19 +8,22 @@ namespace TwoD
     public class UIManager : MonoBehaviour
     {
         public PlayerStatBar playerStatBar;
+        public PlayerStatBar bossStatBar;
 
         [Header("事件監聽")]
         public HealthSystemEventSO healthEvent;
+        public HealthSystemEventSO bossHealthEvent;
 
         private void OnEnable()
         {
             healthEvent.OnEventRaised += OnHealthEvent;
+            bossHealthEvent.OnEventRaised += OnBossHealthEvent;
         }
 
         private void OnDisable()
         {
             healthEvent.OnEventRaised -= OnHealthEvent;
-
+            bossHealthEvent.OnEventRaised -= OnBossHealthEvent;
         }
 
         private void OnHealthEvent(HealthSystem healthSystem)
@@ -28,6 +31,10 @@ namespace TwoD
             var persentage = healthSystem.currentHealth / healthSystem.maxHealth;
             playerStatBar.OnHealthChange(persentage);
         }
-
+        private void OnBossHealthEvent(HealthSystem healthSystem)
+        {
+            var persentage = healthSystem.currentHealth / healthSystem.maxHealth;
+            bossStatBar.OnHealthChange(persentage);
+        }
     }
 }
