d95bbbff3974d7c54b87ba883d5cefaf052e169e
HsuanShi <138540116+HsuanShi@users.noreply.github.com>
2023-11-16 16:27:07 +0800

    Update 新增鑰匙提示(製作中)

---
+++ Assets/共用資料/Script/Item/CheckKey.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CheckKey : MonoBehaviour
+{
+    private GameObject checkKeyDialog;
+    void Start()
+    {
+        checkKeyDialog = GameObject.Find("KeyCheck");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !InventoryManager.HasKey())
+        {
+            checkKeyDialog.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}
